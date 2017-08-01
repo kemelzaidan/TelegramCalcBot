145cb45849264ca9a6c2060ba0672b6d24a9cd69
Kemel Zaidan <kemelzaidan@gmail.com>
2017-08-01 10:31:41 -0300

    working implementation

--- handler.js
+++ handler.js
@@ -2,32 +2,17 @@
 //npm libs
 var math = require('mathjs');
 var request = require('request');
-// request lib options
-const options = {
-  headers: {
-    'Content-type': 'application/json'
-  }
-}
-// telegram api endpoint
-const url = 'https://api.telegram.org/bot439324808:AAEUxvBeHEnnq3UVUPS5CAwQ1is5xXq-kPQ/'
 
-// default callback for requests
-function requestCallback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var success = JSON.parse(body);
-    console.log('Request successfull: ', success);
-  }
-  else {
-    console.error('Something went wrong: ', error);
-  }
-}
+// telegram api endpoint
+const uri = 'https://api.telegram.org/bot439324808:AAEUxvBeHEnnq3UVUPS5CAwQ1is5xXq-kPQ/'
+
 // default handler for POST on '/'
 module.exports.calculate = (event, context, callback) => {
   callback(null, {statusCode: 200});
 
-  console.log('Received an update:\n\n', event);
+  console.log('Received an update: ', event.body);
   
-  let update = event;
+  let update = JSON.parse(event.body);
   let message = update.message
   // let inline_query = update.inline_query
 
@@ -36,6 +21,10 @@ module.exports.calculate = (event, context, callback) => {
     console.log('ANSWER: ', answer);
     
     const answerMessage = {
+      url: uri + 'sendMessage',
+      headers: {
+        'Content-type': 'application/json'
+      },
       body: JSON.stringify({
         chat_id: message.chat.id,
         text: answer,
@@ -43,6 +32,15 @@ module.exports.calculate = (event, context, callback) => {
       })
     }
 
-    request.post(url+'sendMessage', options, requestCallback);
+    console.log('Answer Message: ', answerMessage);
+    request.post(answerMessage, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var success = JSON.parse(body);
+        console.log('Request successfull: ', success);
+      }
+      else {
+        console.error('Something went wrong: ', response);
+      }
+    });
   }
 };
